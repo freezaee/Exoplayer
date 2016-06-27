9b2886c4cddb4b885896ac28b8ac61929d8c32d2
Elvis.He <heyanming2001@163.com>
2016-06-27 12:37:15 +0800

    init 5

--- library/src/main/java/com/google/android/exoplayer/drm/DrmInitData.java
+++ library/src/main/java/com/google/android/exoplayer/drm/DrmInitData.java
@@ -15,13 +15,73 @@
  */
 package com.google.android.exoplayer.drm;
 
-
 import com.google.android.exoplayer.util.Assertions;
+
+import android.media.MediaDrm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Encapsulates initialization data required by a {@link MediaDrm} instances.
  */
 public interface DrmInitData {
+
+  /**
+   * Retrieves initialization data for a given DRM scheme, specified by its UUID.
+   *
+   * @param schemeUuid The DRM scheme's UUID.
+   * @return The initialization data for the scheme, or null if the scheme is not supported.
+   */
+  public abstract SchemeInitData get(UUID schemeUuid);
+
+  /**
+   * A {@link DrmInitData} implementation that maps UUID onto scheme specific data.
+   */
+  public static final class Mapped implements DrmInitData {
+
+    private final Map<UUID, SchemeInitData> schemeData;
+
+    public Mapped() {
+      schemeData = new HashMap<>();
+    }
+
+    @Override
+    public SchemeInitData get(UUID schemeUuid) {
+      return schemeData.get(schemeUuid);
+    }
+
+    /**
+     * Inserts scheme specific initialization data.
+     *
+     * @param schemeUuid The scheme UUID.
+     * @param schemeInitData The corresponding initialization data.
+     */
+    public void put(UUID schemeUuid, SchemeInitData schemeInitData) {
+      schemeData.put(schemeUuid, schemeInitData);
+    }
+
+  }
+
+  /**
+   * A {@link DrmInitData} implementation that returns the same initialization data for all schemes.
+   */
+  public static final class Universal implements DrmInitData {
+
+    private SchemeInitData data;
+
+    public Universal(SchemeInitData data) {
+      this.data = data;
+    }
+
+    @Override
+    public SchemeInitData get(UUID schemeUuid) {
+      return data;
+    }
+
+  }
 
   /**
    * Scheme initialization data.
@@ -45,6 +105,25 @@ public interface DrmInitData {
       this.mimeType = Assertions.checkNotNull(mimeType);
       this.data = Assertions.checkNotNull(data);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof SchemeInitData)) {
+        return false;
+      }
+      if (obj == this) {
+        return true;
+      }
+
+      SchemeInitData other = (SchemeInitData) obj;
+      return mimeType.equals(other.mimeType) && Arrays.equals(data, other.data);
+    }
+
+    @Override
+    public int hashCode() {
+      return mimeType.hashCode() + 31 * Arrays.hashCode(data);
+    }
+
   }
 
 }
